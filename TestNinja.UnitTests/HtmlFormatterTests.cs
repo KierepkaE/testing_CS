cc6e46ec4fab9c5da847fc1ae33f3dbd75622b89
kierepkae <kierepkaedyta@gmail.com>
2019-12-05 12:47:02 +0000

    * HtmlFormatterTests.cs: add test for HtmlFormatter FormatAsBold   method
    * TestNinja.UnitTests.csproj:

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement() {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+
+            // Specific assertion
+
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+
+        }
+    }
+}
