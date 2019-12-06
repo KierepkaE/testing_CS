cb24bd96b7459f840aed1b76beacaca9a1e57e25
kierepkae <kierepkaedyta@gmail.com>
2019-12-06 10:07:09 +0000

    add test for Log method in ErrorLogger class

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,18 @@
+﻿using TestNinja.Fundamentals;
+using NUnit.Framework;
+namespace TestNinja.UnitTests
+{
+    public class ErrorLoggerTests
+    {
+
+        [Test]
+
+        public void Log_WhenCalled_SetTheLastErrorProperty() {
+            var logger = new ErrorLogger();
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo('a'));
+        }
+       
+    }
+}
