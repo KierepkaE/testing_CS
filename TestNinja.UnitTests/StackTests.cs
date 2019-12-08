8aeb615303050a94197692293c59a2d5fc128324
kierepkae <kierepkaedyta@gmail.com>
2019-12-08 12:21:17 +0000

    add test for Count property in Stack class

--- TestNinja.UnitTests/StackTests.cs
+++ TestNinja.UnitTests/StackTests.cs
@@ -5,13 +5,34 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class StackTests
     {
-        [Test]
 
+        [Test]
         public void Push_ArgsIsNull_ThrowArgumentNullException()
         {
 
             var stack = new Stack<string>();
             Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+
+        }
+
+
+
+        [Test]
+        public void Push_ValidArgument_AddTheObjectToTheStack()
+        {
+
+            var stack = new Stack<string>();
+            stack.Push("a");
+            Assert.That(stack.Count, Is.EqualTo(1));
+
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+
+            var stack = new Stack<string>();
+            Assert.That(stack.Count, Is.EqualTo(0));
 
         }
     }
