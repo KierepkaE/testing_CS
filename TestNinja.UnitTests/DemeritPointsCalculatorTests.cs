70f8deced3dd39ac402a8eb8c3fb5401053f4b0c
kierepkae <kierepkaedyta@gmail.com>
2019-12-08 11:44:04 +0000

    * DemeritPointsCalculatorTests.cs: add tests for   DemeritPointsCalculator class
    * TestNinja.UnitTests.csproj: create DemeritPointsCalculatorTests.cs

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+namespace TestNinja.UnitTests
+{
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ReturnArgumentOutOfRangeException(int speed) {
+
+            var calculator = new DemeritPointsCalculator();
+
+            Assert.That(() => calculator.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+      
+
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(60, 0)]
+        [TestCase(61, 0)]
+        [TestCase(62, 0)]
+        [TestCase(70, 1)]
+        [TestCase(80, 3)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemetirPoints(int speed, int result)
+        {
+
+            var calculator = new DemeritPointsCalculator();
+            var points = calculator.CalculateDemeritPoints(speed);
+
+            Assert.That(points, Is.EqualTo(result));
+        }
+    }
+}
+  
