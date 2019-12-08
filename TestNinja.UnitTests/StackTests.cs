599a2b8e4603724007ebbf361a75a1c32ab3e9b4
kierepkae <kierepkaedyta@gmail.com>
2019-12-08 12:17:58 +0000

    create test for passing null arugment to Push method in Stack class

---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,18 @@
+﻿using TestNinja.Fundamentals;
+using NUnit.Framework;
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+
+        public void Push_ArgsIsNull_ThrowArgumentNullException()
+        {
+
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+
+        }
+    }
+}
