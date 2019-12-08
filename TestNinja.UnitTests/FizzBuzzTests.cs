146e4361a451fbb93a13bd83a29f56198949bc38
kierepkae <kierepkaedyta@gmail.com>
2019-12-08 11:16:25 +0000

    add test for GetOutput when input is a number divisible by 3

--- TestNinja.UnitTests/FizzBuzzTests.cs
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -8,12 +8,25 @@ namespace TestNinja.UnitTests
     {
         [Test]
 
-        public void GetOutput_WhenCalledWithNumberDivisibleByFiveAndThree_ReturnFizzBuzz()
+        public void GetOutput_InputIsDivisibleBy5And3_ReturnFizzBuzz()
         {
             var result = FizzBuzz.GetOutput(15);
 
             Assert.That(result, Is.EqualTo("FizzBuzz"));
 
         }
+
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+
+        }
+
+
     }
+}
 }
