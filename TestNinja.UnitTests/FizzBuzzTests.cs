ffc6c57d2055ab19e5043f7d31f3392b77af4afd
kierepkae <kierepkaedyta@gmail.com>
2019-12-08 11:07:34 +0000

    * FizzBuzzTests.cs: add test for FizzBuzz method GetOutput when   number is divisible by both 5 and 3
    * FizzBuzzTests.json: fix error
    * TestNinja.UnitTests.csproj: add test for FizzBuzz method GetOutput when
      number is divisible by both 5 and 3

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,19 @@
+﻿using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+
+namespace TestNinja.UnitTests
+{
+    public class FizzBuzzTests
+    {
+        [Test]
+
+        public void GetOutput_WhenCalledWithNumberDivisibleByFiveAndThree_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+
+        }
+    }
+}
